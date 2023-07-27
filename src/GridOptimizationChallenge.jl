1bfe8ed2497c165b91c4d839bf9c91b1690e611d
Juan Luis Barberia <jbarberia@frba.utn.edu.ar>
2023-07-27 08:41:39 -0300

    find_network and exception tests

--- src/GridOptimizationChallenge.jl
+++ src/GridOptimizationChallenge.jl
@@ -6,22 +6,34 @@ using Artifacts
 const GOC = joinpath(artifact"GridOptimizationChallenge","GridOptimizationChallengeCases-1.0")
 
 """
-    grid_optimization_challenge(fname::AbstractString, path::AbstractString)
+    grid_optimization_challenge(network::AbstractString, scenario::AbstractString)
 
     open a scenario for the Grid Optimization Challenge for the real time cases.
 """
-function grid_optimization_challenge(scenario::AbstractString, network::AbstractString)
-    _grid_optimization_challenge(scenario::AbstractString, network::AbstractString)
+function grid_optimization_challenge(network::AbstractString, scenario::AbstractString)
+    _grid_optimization_challenge(network::AbstractString, scenario::AbstractString)
 end
 
-function _grid_optimization_challenge(scenario::AbstractString, network::AbstractString)
+function _grid_optimization_challenge(network::AbstractString, scenario::AbstractString)
     if isdir(joinpath(GOC, network, scenario))
         case = parse_directory(joinpath(GOC, network, scenario))
+    elseif !isdir(joinpath(GOC, network))
+        error("Network not found")
+    elseif !isdir(joinpath(GOC, network, scenario))
+        error("Scenario not found")
     else
-        error("scenario not found")
+        error("Invalid network and scenario")
     end
 end
 
-export grid_optimization_challenge
+function find_network(network_name::AbstractString)
+    network_folders = readdir(GOC)
+    filter!(x -> isdir(joinpath(GOC, x)), network_folders)
+    filtered_networks = filter(x -> occursin(network_name, x), network_folders)
+
+    return filtered_networks
+end
+
+export grid_optimization_challenge, find_network
 
 end # module GridOptimizationChallenge
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,15 @@ using Test
 case = parse_directory("Network_02R-173/scenario_1/")
 
 @testset "grid-optimization-challenge" begin
-    @test case == grid_optimization_challenge("scenario_1", "Network_02R-173")
+    @test case == grid_optimization_challenge("Network_02R-173", "scenario_1")
 end
+
+@testset "errors-and-exceptions" begin
+    @test_throws "Network not found" grid_optimization_challenge("Invalid Network", "")
+    @test_throws "Scenario not found" grid_optimization_challenge("Network_02R-173", "Invalid Scenario")    
+end
+
+@testset "find-files" begin
+    @test length(find_network("Network")) == 18
+end
+
